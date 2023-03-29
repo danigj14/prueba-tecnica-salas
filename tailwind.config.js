6df9dec48eef05e3d525b5536b0f8043080197a6
Dani <24252795+SGD14@users.noreply.github.com>
2023-03-29 17:40:38 +0200

    Add commented Helvetica font setting.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        //"sans": ["Helvetica"]
+      }
+    },
     colors: {
       "white": "#FFFFFF",
       "black": "#000000",
