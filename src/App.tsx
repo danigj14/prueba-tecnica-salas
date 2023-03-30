5886e4ace94bdd8abbf2527716f087f43a315402
Dani <24252795+SGD14@users.noreply.github.com>
2023-03-30 09:59:12 +0200

    Add floor selection state

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
+import { useState } from "react";
 import { Select } from "./core/components";
 import { FloorPanel } from "./features/rooms";
 
 export default function App() {
+  const [selectedFloor, setSelectedFloor] = useState(1);
+
   return (
     <div className="text-blue-dark container xl:max-w-screen-xl mx-auto p-4">
       <h1 className="mb-5 text-3xl font-bold">Salas</h1>
-      <Select className="mb-5">
-        <option>Planta 1</option>
-        <option>Planta 2</option>
-        <option>Planta 3</option>
-        <option>Planta 4</option>
+      <Select
+        className="mb-5"
+        onChange={(event) =>
+          setSelectedFloor(Number.parseInt(event.target.value))
+        }
+      >
+        <option value="1">Planta 1</option>
+        <option value="2">Planta 2</option>
+        <option value="3">Planta 3</option>
+        <option value="4">Planta 4</option>
       </Select>
       <div className="pt-5 border-t-2 border-blue-light"></div>
-      <FloorPanel />
+      <FloorPanel floor={selectedFloor}/>
     </div>
   );
 }
--- src/features/rooms/components/FloorPanel.tsx
+++ src/features/rooms/components/FloorPanel.tsx
@@ -1,11 +1,15 @@
 import { Button } from "@/core/components";
 import { RoomCard } from "./RoomCard";
 
-export function FloorPanel() {
+interface FloorPanelProps {
+  floor: number;
+}
+
+export function FloorPanel({floor}: FloorPanelProps) {
   return (
     <div className="p-4 border border-blue-dark rounded-[26px]">
       <div className="flex justify-between items-center">
-        <h1 className="text-xl font-bold">Planta 1</h1>
+        <h1 className="text-xl font-bold">Planta {floor}</h1>
         <Button>Añadir sala</Button>
       </div>
       <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
